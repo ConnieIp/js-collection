e55e07a8876c5cde28af60b37c5465878f872fc2
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 14:30:50 +0800

    map_to_even

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -2,10 +2,11 @@
 
 function map_to_even(collection) {
     // return [2,4,6,8,10];
-    let result = [];
-    for (let i = 0; i < collection.length; i++) {
-        result.push(collection[i] * 2);
-    }
-    return result;
+    // let result = [];
+    // for (let i = 0; i < collection.length; i++) {
+    //     result.push(collection[i] * 2);
+    // }
+    // return result;
+    return collection.map(a => a * 2);
 }
 module.exports = map_to_even;
