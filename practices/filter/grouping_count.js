c5852879a5f92d5c30708818b3fc7a710d8ad416
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 17:12:31 +0800

    group count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,15 +3,27 @@
 function grouping_count(collection) {
 
     //implement here
+    // let result = {};
+    // for (let i = 0; i < collection.length; i++) {
+    //     if (result[collection[i]] != null) {
+    //         result[collection[i]] = result[collection[i]] + 1;
+    //     } else {
+    //         result[collection[i]] = 1;
+    //     }
+    // }
+    // return result;
+
     let result = {};
-    for (let i = 0; i < collection.length; i++) {
-        if (result[collection[i]] != null) {
-            result[collection[i]] = result[collection[i]] + 1;
-        } else {
-            result[collection[i]] = 1;
-        }
+    let array = collection.filter((a, index, self) => self.indexOf(a) === index);
+    for (let i = 0; i < array.length; i++) {
+        result[array[i]] = collection.filter(b => b === array[i]).length;
     }
     return result;
+
+    // let result = {};
+    // let array = collection.filter((a, index, self) => self.indexOf(a) === index);
+    // return new Map(array.map(c => [c, collection.filter(b => b === c).length]));
+
 }
 
 module.exports = grouping_count;
