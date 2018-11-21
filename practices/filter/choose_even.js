e643efa215fbdea13feffd85c44585fb2d89058b
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 14:18:36 +0800

    choose_even filter

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,14 +3,14 @@
 function choose_even(collection) {
 
     //implement here
-    let result = [];
-    for (let i = 0; i < collection.length; i++) {
-        if (collection[i] % 2 === 0) {
-            result.push(collection[i]);
-        }
-    }
-    return result;
-    // return collection.filter(a => a % 2 === 0);
+    // let result = [];
+    // for (let i = 0; i < collection.length; i++) {
+    //     if (collection[i] % 2 === 0) {
+    //         result.push(collection[i]);
+    //     }
+    // }
+    // return result;
+    return collection.filter(a => a % 2 === 0);
 }
 
 module.exports = choose_even;
