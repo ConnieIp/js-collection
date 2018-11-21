a8ae97d30d6fcff0bdd29aeca588f251bb2b47c9
Connie Ip <connie.ip@cargosmart.com>
2018-11-21 14:17:08 +0800

    choose_even for loop

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -2,7 +2,15 @@
 
 function choose_even(collection) {
 
-  //implement here
+    //implement here
+    let result = [];
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] % 2 === 0) {
+            result.push(collection[i]);
+        }
+    }
+    return result;
+    // return collection.filter(a => a % 2 === 0);
 }
 
 module.exports = choose_even;
